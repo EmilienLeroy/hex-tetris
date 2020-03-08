2aed748e059393ff981557a30870df0ca37d4b33
Emilien <admin@emilienleroy.fr>
2020-03-08 21:41:06 +0100

    :fire: Remove useless WallFactory

--- src/Layout/index.ts
+++ src/Layout/index.ts
@@ -1,14 +1,9 @@
 import { useType, useChild, Vector } from '@hex-engine/2d';
-import WallFactory from './WallFactory';
+import Wall from './Wall';
 
 export default function Layout(width: number, height: number, center: Vector) {
-    useType(Layout);
-
-    const wallLeft = WallFactory(25, height, new Vector(center.x - width / 2, center.y));
-    const wallRight = WallFactory(25, height, new Vector(center.x + width / 2, center.y));
-    const wallBottom = WallFactory(width, 25, new Vector(center.x, height - 8));
-    
-    useChild(() => wallLeft());
-    useChild(() => wallRight());
-    useChild(() => wallBottom())
+    useType(Layout);    
+    useChild(() => Wall(25, height, new Vector(center.x - width / 2, center.y)));
+    useChild(() => Wall(25, height, new Vector(center.x + width / 2, center.y)));
+    useChild(() => Wall(width, 25, new Vector(center.x, height - 8)))
 }
